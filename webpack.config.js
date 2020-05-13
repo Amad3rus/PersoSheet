38b1424fa9f36332325cfdcc7212c129fa140b6e
Amad3rus <vini-braine@hotmail.com>
2020-05-13 14:12:07 -0300

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+        template:'.public/index.html'
+});
+
+module.exports = {
+  
+   "entry": './index.js',
+   "output":{
+     "filename":'bundle.js',
+     "path": path.resolve(__dirname, '/dist'),
+     "publicPath":'/'
+   },
+
+   "module":{
+     "rules":[
+         {
+           "test":/\.js$/i,
+	   "exclude":/node_modules/,
+	   "loader":'babel-loader',
+           "query":{
+               "presets": ['es2015', 'stage-0'],
+	       "compact": true
+	   }
+	 },
+	 {
+            "test":/\.css$/i,
+	    "use":['style-loader', 'css-loader']
+	 }
+     ]
+   },
+
+   },
+   "plugin": [
+     HTMLWebpackPluginConfig
+   ]
+}

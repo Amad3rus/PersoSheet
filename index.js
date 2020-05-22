249cff70d0c2ce4ec88e187fb607594209e36cc6
Amad3rus <63552833+Amad3rus@users.noreply.github.com>
2020-05-22 14:20:51 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
   
   document.getElementById("btn1").addEventListener("click",function(){
-    var box1=document.getElementById("login-card");
+    var box1=document.getElementById("login");
     if (box1.style.display=="none") {
       box1.style.display="block";
     }else{

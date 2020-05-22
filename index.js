88e3a7201970417d81e4b3c2e981960c8297b282
Amad3rus <63552833+Amad3rus@users.noreply.github.com>
2020-05-22 14:18:53 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
   
-  
+  document.getElementById("btn1").addEventListener("Enter",function(){
+    var box1=document.getElementById("login-card");
+    if (box1.style.display=="none") {
+      box1.style.display="block";
+    }else{
+      box1.style.display="none";
+    }
+  })
 
+/*
   var slider = document.getElementById("myRange");
   var output = document.getElementById("value");
 
@@ -9,3 +17,4 @@
   slider.oninput = function() {
   output.innerHTML = this.value;
   }
+  */
